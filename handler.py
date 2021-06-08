0feb623fd816fac5b7bfdbd46620e0e912552839
Ben Robinson <ben.robinson@invitae.com>
2021-06-08 08:30:07 -0400

    running autoformatter and tidying some things

--- handler.py
+++ handler.py
@@ -4,8 +4,6 @@ import hmac
 import hashlib
 import os
 
-import requests
-
 # setup logging
 logger = logging.getLogger()
 if logger.handlers:
@@ -14,19 +12,17 @@ if logger.handlers:
 logging.basicConfig(level=logging.INFO)
 
 OK_RESPONSE = {
-    'statusCode': 200,
-    'headers': {'Content-Type': 'application/json'},
-    'body': json.dumps('ok')
+    "statusCode": 200,
+    "headers": {"Content-Type": "application/json"},
+    "body": json.dumps("ok"),
 }
-ERROR_RESPONSE = {
-    'statusCode': 400,
-    'body': json.dumps('Oops, something went wrong!')
-}
+ERROR_RESPONSE = {"statusCode": 400, "body": json.dumps("Oops, something went wrong!")}
 
 UNAUTH_REPSONSE = {
-    'statusCode': 403,
-    'body': json.dumps('Signature Verification Failed')
+    "statusCode": 403,
+    "body": json.dumps("Signature Verification Failed"),
 }
+
 
 def configure_message():
     """
@@ -34,34 +30,37 @@ def configure_message():
     Returns a bot instance.
     """
 
+
 def notification(event, context):
     """
     Receives notification from API Gateway
     """
-    logger.info(f'Event: {event}')
+    logger.info(f"Event: {event}")
 
     return OK_RESPONSE
+
 
 def eventsub_callback(event, context):
     """
     Receives eventsub callback from API Gateway
     """
-    def verify_signature(event, headers):
-        headers = event.get('headers')
-        message = headers['Twitch-Eventsub-Message-Id'] + headers['Twitch-Eventsub-Message-Timestamp'] + event.get('body')
-        key = os.environ.get('CALLBACK_SECRET')
-        computed_sig = hmac.new(os.environ.get('CALLBACK_SECRET').encode(), msg=b'message', digestmod=hashlib.sha256).hexdigest()
-        expected_header = f'sha256={computed_sig}'
-        return expected_header == headers['Twitch-Eventsub-Message-Signature']
 
-    headers = event.get('headers')
-    if headers['Twitch-Eventsub-Message-Type'] == 'webhook_callback_verification':
-        is_valid = verify_signature(event, headers)
-        if is_valid:
-            logger.info('Signature verified!')
+    def is_signature_valid(event, headers):
+        headers = event.get("headers")
+        key = os.environ.get("CALLBACK_SECRET")
+        computed_sig = hmac.new(
+            key.encode(), msg=b"message", digestmod=hashlib.sha256,
+        ).hexdigest()
+        expected_header = f"sha256={computed_sig}"
+        return expected_header == headers["Twitch-Eventsub-Message-Signature"]
+
+    headers = event.get("headers")
+    if headers["Twitch-Eventsub-Message-Type"] == "webhook_callback_verification":
+        if is_signature_valid(event, headers):
+            logger.info("Signature verified!")
             return OK_RESPONSE
         else:
-            logger.warn('Signature verification failed!')
+            logger.warn("Signature verification failed!")
             return UNAUTH_REPSONSE
 
     return ERROR_RESPONSE
